e63fa6b197abf066f54b33aba79010e3a3baca6c
khulet <khulet@iastate.edu>
2023-12-13 22:18:22 -0600

    Added to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import { ConfigProvider } from "antd";
+import React from "react";
+// import "./App.css";
 
-function App() {
-  return (
-    <div>
-      Jim is the best coder ever
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <ConfigProvider theme={{ token: { colorPrimary: "#00b96b" } }}>
+    <div>FORTNITEFORTNITEFORNITEOFRNITE</div>
+  </ConfigProvider>
+);
 
 export default App;
